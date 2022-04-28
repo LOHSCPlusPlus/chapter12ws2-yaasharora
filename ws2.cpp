ccfb36dcc6b83b7683d0b5dc9fcb20320a97ae1d
YASHARORA6 <>
2022-04-28 14:54:09 +0000

    Done

--- ws2.cpp
+++ ws2.cpp
@@ -5,17 +5,19 @@
 using namespace std;
 
 // Your job is to fix the errors in each function
-// build using make -f ws1.mak
+// build using make -f ws2.mak
 // run under valgrind using
-// valgrind --leak-check=full ws1
+// valgrind --leak-check=full ./ws2
 // You have a section fixed when valgrind says you have no errors.
 
 // There are two errors in this first block
 void PartB() {
     int *arrayPtr = new int[700];
-    for (int i = 0; i <= 700; i++) {
+    for (int i = 0; i < 700; i++) {
         arrayPtr[i] = i;
-    }
+      }
+      cout << "arrayPtr[700] = " << arrayPtr[20] << endl;
+    delete [] arrayPtr;
 }
 
 // Look in arrayExamples.cpp createCharPointer
@@ -23,10 +25,12 @@ void PartB() {
 // There is only one actual error in here, but it causes
 // valgrind to detect two issues.
 void PartC() {
-    int len = strlen("Hello");
+    int len = strlen("Hello") + 1;
     char *a = new char[len];
     strcpy(a, "Hello");
     cout << a << endl;
+
+  delete [] a;
 }
 
 
@@ -36,7 +40,9 @@ void PartD() {
     int *ptr2 = new int [10];
     for (int i = 0; i < 10; i++) {
         ptr1[i] = i;
+
     }
+  
     // These prints are here to help you. We do not want to change
     // where ptr2 is pointing, we want to change the contents of the array.
     cout << "ptr2 is pointing at: " << ptr2 << endl;
@@ -44,16 +50,18 @@ void PartD() {
     // contents of the array in ptr1 into the array in ptr2.
     // They wanted two separate arrays each with the same values.
     // Fix it so that is what we have.
-    ptr2 = ptr1;
     cout << "ptr2 is pointing at: " << ptr2 << endl;
-    if (ptr2[3] == 3) {
+  for (int i = 0; i < 10; i++) {
+    ptr2[i]=ptr1[i];
+    }   
+  if (ptr2[3] == 3) {
         cout << "passed" << endl;
     }
     else {
         cout << "failed" << endl;
     }
     delete [] ptr1;
-    //delete [] ptr2;
+    delete [] ptr2;
 }
 // In this case, we do want the two pointers to point to the same
 // array. But, we still messed something up. line 66 is a problem. Why?
@@ -63,18 +71,18 @@ void PartE() {
     for (int i = 0; i < 10; i++) {
         ptr1[i] = i;
     }
-    delete [] ptr1;
-    ptr1 = nullptr;
     for (int i = 0; i < 10; i++) {
         cout << ptr2[i]  << endl;
     }
+     delete [] ptr1;
+     ptr1 = nullptr;
 }
 
 int main() {
-    //PartB();  
-    //PartC();  
-    //PartD();  
-    //PartE();  
+    PartB();  
+    PartC();  
+    PartD();  
+    PartE();  
 
 
 }
